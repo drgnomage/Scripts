18ab051e0bb289e8e0a0d29cfc77b478ec6aa3da
Josh Fowler <josh.fowler@securus-software.com>
2019-11-04 16:14:10 +0000

    Updated zram.sh.

--- zram.sh
+++ zram.sh
@@ -5,10 +5,14 @@ if ! whereis zramctl > /dev/null ; then
 	break
 fi
 
+if [ -z "$(lsmod | grep -i zram)" ] ; then
+	modprobe zram num_devices=2
+fi
+
 mem=$(free -m | grep Mem | awk '{print $2}')
 swap=$(free -m | grep Swap | awk '{print $2}')
 difference=$(($mem - $swap))
-required=$(($mem/4))
+required=$(($mem/1))
 
 if [[ $swap -lt $required ]] ; then
 	echo -e "Mem:\t$mem\nSwap:\t$swap\nDiff:\t$difference\nReq:\t$required\n"
