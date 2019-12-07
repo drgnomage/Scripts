acd6c687ba2ed096d55a718ff67ca8508f4dbb04
Josh Fowler <joshfowler@protonmail.com>
2019-12-07 09:36:51 +0000

    Changed WOL_media.sh to not specify the IP address so that WOL will work after 4 hours.

---
+++ WOL_media.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+<<<<<<< HEAD
+#wakeonlan -i 192.168.1.1 -p9 "f4:6d:04:62:b0:d7"
+wakeonlan "f4:6d:04:62:b0:d7"
+=======
+wakeonlan -i 192.168.1.1 -p9 "f4:6d:04:62:b0:d7"
+>>>>>>> 86927f30c7fa220cf02d838d9b251f6c2b2c2fef
