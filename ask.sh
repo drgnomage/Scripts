46e03eb3ccedad17b05a428a940b7ea8c4b0bd4f
Josh Fowler <joshfowler@protonmail.com>
2021-02-06 09:39:57 +0000

    Removed some old and useless scripts.

--- ask.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-awk 'NR==FNR{a["{[^{}]*"$0"[^{}]*}"]++;next}{for(i in a)if($0~i)next;b[j++]=$0}END{printf "">FILENAME;for(i=0;i in b;++i)print b[i]>FILENAME}' ~/mnt/160503\ Customer\ Cancelled\ List.csv ~/mnt/160503\ Edubase\ Data\ for\ Josh.csv
--- posttorrent-9090.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-{
-	/home/josh/.bin/transmission_purge.sh
-} &
